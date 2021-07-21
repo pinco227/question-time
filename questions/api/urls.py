9d49a6fdfd0f0dea4013cf5f7443d5224646c7e3
Paul Istratoaie <p4ul.gg@gmail.com>
2021-07-21 12:47:49 +0100

    Add answer create and list api views and urls

--- questions/api/urls.py
+++ questions/api/urls.py
@@ -7,4 +7,12 @@ router.register(r'questions', qv.QuestionViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+
+    path('questions/<slug:slug>/answers/',
+         qv.AnswerListAPIView.as_view(),
+         name='answer-list'),
+
+    path('questions/<slug:slug>/answer/',
+         qv.AnswerCreateAPIView.as_view(),
+         name='create-answer'),
 ]
--- questions/api/views.py
+++ questions/api/views.py
@@ -1,7 +1,9 @@
 from questions.api.permissions import IsAuthorOrReadOnly
-from questions.api.serializers import QuestionSerializer
-from questions.models import Question
-from rest_framework import viewsets
+from questions.api.serializers import AnswerSerializer, QuestionSerializer
+from questions.models import Answer, Question
+from rest_framework import generics, viewsets
+from rest_framework.generics import get_object_or_404
+from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated
 
 
@@ -13,3 +15,29 @@ class QuestionViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+
+class AnswerCreateAPIView(generics.CreateAPIView):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        request_user = self.request.user
+        kwarg_slug = self.kwargs.get('slug')
+        question = get_object_or_404(Question, slug=kwarg_slug)
+
+        if question.answers.filter(author=request_user).exists():
+            raise ValidationError("You have already answered this question")
+
+        serializer.save(author=request_user, question=question)
+
+
+class AnswerListAPIView(generics.ListAPIView):
+    serializer_class = AnswerSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        kwarg_slug = self.kwargs.get('slug')
+        return Answer.objects.filter(
+            question__slug=kwarg_slug).order_by('-created_at')
