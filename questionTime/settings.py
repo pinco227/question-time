ed0be38f2276e740fcdc6c3e84af78d6804c0446
Paul Istratoaie <p4ul.gg@gmail.com>
2021-07-22 10:59:27 +0100

    Static root settings for heroku

--- questionTime/settings.py
+++ questionTime/settings.py
@@ -146,7 +146,13 @@ LOGOUT_REDIRECT_URL = '/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
