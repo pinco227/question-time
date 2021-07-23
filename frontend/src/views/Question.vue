51455dde2f824d654e75f4afc0373f4e2f60b8c3
Paul Istratoaie <p4ul.gg@gmail.com>
2021-07-23 12:20:30 +0100

    Create answer question functionality

--- frontend/src/views/Question.vue
+++ frontend/src/views/Question.vue
@@ -6,6 +6,40 @@
       {{ question.created_at }}
     </p>
     <hr />
+    <div v-if="question.user_has_answered">
+      <p class="answer-added">You've answered this question.</p>
+    </div>
+    <div v-else-if="showForm">
+      <form class="card" @submit.prevent="onSubmit">
+        <div class="card-header px-3">Share your knowledge!</div>
+        <textarea
+          rows="5"
+          v-model="newAnswerBody"
+          class="card-body"
+          placeholder="Write your answer...."
+        ></textarea>
+        <div class="card-body bg-danger text-white fw-bold" v-if="error">
+          <p class="m-0">
+            <i class="fas fa-exclamation-triangle"></i> {{ error }}
+          </p>
+        </div>
+        <div class="card-footer px-3 d-flex justify-content-between">
+          <button type="submit" class="btn btn-sm btn-success">Submit</button>
+          <button
+            class="btn btn-sm btn-outline-danger"
+            @click="showForm = false"
+          >
+            Cancel
+          </button>
+        </div>
+      </form>
+    </div>
+    <div v-else>
+      <button class="btn btn-sm btn-success" @click="showForm = true">
+        Answer the Question
+      </button>
+    </div>
+    <hr />
     <AnswerComponent
       v-for="(answer, index) in answers"
       :key="index"
@@ -32,6 +66,9 @@ export default {
     return {
       question: {},
       answers: [],
+      newAnswerBody: null,
+      error: null,
+      showForm: false,
     };
   },
   methods: {
@@ -41,8 +78,9 @@ export default {
     getQuestionData() {
       let endpoint = `/api/questions/${this.slug}/`;
       apiService(endpoint).then((data) => {
-        this.question = data;
         this.setPageTitle(data.content);
+        this.question = data;
+        // this.question.user_has_answered = data.user_has_answered;
       });
     },
     getQuestionAnswers() {
@@ -50,6 +88,25 @@ export default {
       apiService(endpoint).then((data) => {
         this.answers = data.results;
       });
+    },
+    onSubmit() {
+      if (this.newAnswerBody) {
+        let endpoint = `/api/questions/${this.slug}/answer/`;
+        let method = "POST";
+        apiService(endpoint, method, { body: this.newAnswerBody }).then(
+          (data) => {
+            this.answers.unshift(data);
+          }
+        );
+        this.newAnswerBody = null;
+        this.showForm = false;
+        this.question.user_has_answered = true;
+        if (this.error) {
+          this.error = null;
+        }
+      } else {
+        this.error = "You can't submit an empty asnwer!";
+      }
     },
   },
   created() {
@@ -63,5 +120,9 @@ export default {
 .author-name {
   font-weight: bold;
   color: crimson;
+}
+.answer-added {
+  font-weight: bold;
+  color: seagreen;
 }
 </style>
